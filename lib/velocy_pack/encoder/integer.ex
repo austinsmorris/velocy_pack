8d568444b417de159936f2508e0a968f04c423b7
Austin Morris <austin.morris@gmail.com>
2017-05-21 09:50:03 -0400

    encode integers

--- lib/velocy_pack/encoder/integer.ex
+++ lib/velocy_pack/encoder/integer.ex
@@ -16,4 +16,30 @@ defimpl VelocyPack.Encoder, for: Integer do
   def encode(-3, _), do: 0x3d
   def encode(-2, _), do: 0x3e
   def encode(-1, _), do: 0x3f
+
+  def encode(value, _) when value < -9_223_372_036_854_775_808 do
+    raise "Cannot encode integers less than -9_223_372_036_854_775_808."
+  end
+
+  def encode(value, _) when value < -36_028_797_018_963_968, do: <<0x27, value::little-signed-size(64)>>
+  def encode(value, _) when value < -140_737_488_355_328, do: <<0x26, value::little-signed-size(56)>>
+  def encode(value, _) when value < -549_755_813_888, do: <<0x25, value::little-signed-size(48)>>
+  def encode(value, _) when value < -2_147_483_648, do: <<0x24, value::little-signed-size(40)>>
+  def encode(value, _) when value < -8_388_608, do: <<0x23, value::little-signed-size(32)>>
+  def encode(value, _) when value < -32_768, do: <<0x22, value::little-signed-size(24)>>
+  def encode(value, _) when value < -128, do: <<0x21, value::little-signed-size(16)>>
+  def encode(value, _) when value < 0, do: <<0x20, value::little-signed-size(8)>>
+
+  def encode(value, _) when value <= 255, do: <<0x28, value::little-unsigned-size(8)>>
+  def encode(value, _) when value <= 65_535, do: <<0x29, value::little-unsigned-size(16)>>
+  def encode(value, _) when value <= 16_777_215, do: <<0x2a, value::little-unsigned-size(24)>>
+  def encode(value, _) when value <= 4_294_967_295, do: <<0x2b, value::little-unsigned-size(32)>>
+  def encode(value, _) when value <= 1_099_511_627_775, do: <<0x2c, value::little-unsigned-size(40)>>
+  def encode(value, _) when value <= 281_474_976_710_655, do: <<0x2d, value::little-unsigned-size(48)>>
+  def encode(value, _) when value <= 72_057_594_037_927_935, do: <<0x2e, value::little-unsigned-size(56)>>
+  def encode(value, _) when value <= 18_446_744_073_709_551_615, do: <<0x2f, value::little-unsigned-size(64)>>
+
+  def encode(_, _) do
+    raise "Cannot encode integers greater than 18_446_744_073_709_551_615."
+  end
 end
--- test/velocy_pack/encoder/integer_test.exs
+++ test/velocy_pack/encoder/integer_test.exs
@@ -22,4 +22,64 @@ defmodule VelocyPack.Encoder.IntegerTest do
     assert VelocyPack.encode(-2) == 0x3e
     assert VelocyPack.encode(-1) == 0x3f
   end
+
+  test "encode signed integers" do
+    assert VelocyPack.encode(-7) == <<0x20, 249>>
+    assert VelocyPack.encode(-128) == <<0x20, 128>>
+
+    assert VelocyPack.encode(-129) == <<0x21, 127, 255>>
+    assert VelocyPack.encode(-32_768) == <<0x21, 0, 128>>
+
+    assert VelocyPack.encode(-32_769) == <<0x22, 255, 127, 255>>
+    assert VelocyPack.encode(-8_388_608) == <<0x22, 0, 0, 128>>
+
+    assert VelocyPack.encode(-8_388_609) == <<0x23, 255, 255, 127, 255>>
+    assert VelocyPack.encode(-2_147_483_648) == <<0x23, 0, 0, 0, 128>>
+
+    assert VelocyPack.encode(-2_147_483_649) == <<0x24, 255, 255, 255, 127, 255>>
+    assert VelocyPack.encode(-549_755_813_888) == <<0x24, 0, 0, 0, 0, 128>>
+
+    assert VelocyPack.encode(-549_755_813_889) == <<0x25, 255, 255, 255, 255, 127, 255>>
+    assert VelocyPack.encode(-140_737_488_355_328) == <<0x25, 0, 0, 0, 0, 0, 128>>
+
+    assert VelocyPack.encode(-140_737_488_355_329) == <<0x26, 255, 255, 255, 255, 255, 127, 255>>
+    assert VelocyPack.encode(-36_028_797_018_963_968) == <<0x26, 0, 0, 0, 0, 0, 0, 128>>
+
+    assert VelocyPack.encode(-36_028_797_018_963_969) == <<0x27, 255, 255, 255, 255, 255, 255, 127, 255>>
+    assert VelocyPack.encode(-9_223_372_036_854_775_808) == <<0x27, 0, 0, 0, 0, 0, 0, 0, 128>>
+
+    assert_raise(RuntimeError, "Cannot encode integers less than -9_223_372_036_854_775_808.", fn ->
+      VelocyPack.encode(-9_223_372_036_854_775_809)
+    end)
+  end
+
+  test "encode unsigned integers" do
+    assert VelocyPack.encode(10) == <<0x28, 10>>
+    assert VelocyPack.encode(255) == <<0x28, 255>>
+
+    assert VelocyPack.encode(256) == <<0x29, 0, 1>>
+    assert VelocyPack.encode(65_535) == <<0x29, 255, 255>>
+
+    assert VelocyPack.encode(65_536) == <<0x2a, 0, 0, 1>>
+    assert VelocyPack.encode(16_777_215) == <<0x2a, 255, 255, 255>>
+
+    assert VelocyPack.encode(16_777_216) == <<0x2b, 0, 0, 0, 1>>
+    assert VelocyPack.encode(4_294_967_295) == <<0x2b, 255, 255, 255, 255>>
+
+    assert VelocyPack.encode(4_294_967_296) == <<0x2c, 0, 0, 0, 0, 1>>
+    assert VelocyPack.encode(1_099_511_627_775) == <<0x2c, 255, 255, 255, 255, 255>>
+
+    assert VelocyPack.encode(1_099_511_627_776) == <<0x2d, 0, 0, 0, 0, 0, 1>>
+    assert VelocyPack.encode(281_474_976_710_655) == <<0x2d, 255, 255, 255, 255, 255, 255>>
+
+    assert VelocyPack.encode(281_474_976_710_656) == <<0x2e, 0, 0, 0, 0, 0, 0, 1>>
+    assert VelocyPack.encode(72_057_594_037_927_935) == <<0x2e, 255, 255, 255, 255, 255, 255, 255>>
+
+    assert VelocyPack.encode(72_057_594_037_927_936) == <<0x2f, 0, 0, 0, 0, 0, 0, 0, 1>>
+    assert VelocyPack.encode(18_446_744_073_709_551_615) == <<0x2f, 255, 255, 255, 255, 255, 255, 255, 255>>
+
+    assert_raise(RuntimeError, "Cannot encode integers greater than 18_446_744_073_709_551_615.", fn ->
+      VelocyPack.encode(18_446_744_073_709_551_616)
+    end)
+  end
 end
