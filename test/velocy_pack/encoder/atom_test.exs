29005e70f55e36c63a599b199e0e043583bbff8e
Austin Morris <austin.morris@gmail.com>
2017-05-26 21:34:20 -0400

    test for atom encode_with_szie()

--- test/velocy_pack/encoder/atom_test.exs
+++ test/velocy_pack/encoder/atom_test.exs
@@ -1,18 +1,35 @@
 defmodule VelocyPack.Encoder.AtomTest do
+  alias VelocyPack.Encoder
+
   use ExUnit.Case, async: true
 
-  test "encode nil" do
-    assert VelocyPack.encode(nil) == 0x18
-    assert VelocyPack.encode(:nil) == 0x18
+  test "encode() nil" do
+    assert Encoder.encode(nil) == 0x18
+    assert Encoder.encode(:nil) == 0x18
   end
 
-  test "encode false" do
-    assert VelocyPack.encode(false) == 0x19
-    assert VelocyPack.encode(:false) == 0x19
+  test "encode() false" do
+    assert Encoder.encode(false) == 0x19
+    assert Encoder.encode(:false) == 0x19
   end
 
-  test "encode true" do
-    assert VelocyPack.encode(true) == 0x1a
-    assert VelocyPack.encode(:true) == 0x1a
+  test "encode() true" do
+    assert Encoder.encode(true) == 0x1a
+    assert Encoder.encode(:true) == 0x1a
+  end
+
+  test "encode_with_size() nil" do
+    assert Encoder.encode_with_size(nil) == {0x18, 1}
+    assert Encoder.encode_with_size(:nil) == {0x18, 1}
+  end
+
+  test "encode_with_size() false" do
+    assert Encoder.encode_with_size(false) == {0x19, 1}
+    assert Encoder.encode_with_size(:false) == {0x19, 1}
+  end
+
+  test "encode_with_size() true" do
+    assert Encoder.encode_with_size(true) == {0x1a, 1}
+    assert Encoder.encode_with_size(:true) == {0x1a, 1}
   end
 end
