1763b6796c78f6e68e766bc3fa354fc926303097
Austin Morris <austin.morris@gmail.com>
2017-05-26 21:42:02 -0400

    test map encode_with_size()

--- test/velocy_pack/encoder/map_test.exs
+++ test/velocy_pack/encoder/map_test.exs
@@ -1,7 +1,13 @@
 defmodule VelocyPack.Encoder.MapTest do
+  alias VelocyPack.Encoder
+
   use ExUnit.Case, async: true
 
-  test "encode an empty map" do
-    assert VelocyPack.encode(%{}) == 0x0a
+  test "encode() with empty map" do
+    assert Encoder.encode(%{}) == 0x0a
+  end
+
+  test "encode_with_size() with empty map" do
+    assert Encoder.encode_with_size(%{}) == {0x0a, 1}
   end
 end
