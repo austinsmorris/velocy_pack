900550fada164c4f5a18cb55c00d2f6a95da3854
Austin Morris <austin.morris@gmail.com>
2017-05-26 21:57:46 -0400

    bit_string encode_with_size()

--- lib/velocy_pack/encoder/bit_string.ex
+++ lib/velocy_pack/encoder/bit_string.ex
@@ -8,4 +8,12 @@ defimpl VelocyPack.Encoder, for: BitString do
   end
 
   def encode_with_size(<<>>, _), do: {0x40, 1}
+  def encode_with_size(value, _) when is_binary(value) and byte_size(value) <= 126 do
+    size = byte_size(value)
+    {<<(size + 0x40), value::binary>>, (size + 1)}
+  end
+  def encode_with_size(value, _) when is_binary(value) do
+    size = byte_size(value)
+    {<<0xbf, size::little-unsigned-size(64), value::binary>>, (size + 8 + 1)}
+  end
 end
--- test/velocy_pack/encoder/bit_string_test.exs
+++ test/velocy_pack/encoder/bit_string_test.exs
@@ -1,30 +1,60 @@
 defmodule VelocyPack.Encoder.BitStringTest do
+  alias VelocyPack.Encoder
+
   use ExUnit.Case, async: true
 
-  test "encode an empty bit string" do
-    assert VelocyPack.encode(<<>>) == 0x40
+  test "encode() an empty bit string" do
+    assert Encoder.encode(<<>>) == 0x40
   end
 
-  test "encode an empty string" do
-    assert VelocyPack.encode("") == 0x40
+  test "encode() an empty string" do
+    assert Encoder.encode("") == 0x40
   end
 
-  test "encode a short string" do
-    assert VelocyPack.encode("a") == <<0x41, "a">>
-    assert VelocyPack.encode("foo") == <<0x43, "foo">>
+  test "encode() a short string" do
+    assert Encoder.encode("a") == <<0x41, "a">>
+    assert Encoder.encode("foo") == <<0x43, "foo">>
 
     string = "asdlfkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdfasdflkajsdflkjas"
       <> "dfasdfasdfasdfasdfalka"
-    assert VelocyPack.encode(string) == <<0xbe, string::binary>>
+    assert Encoder.encode(string) == <<0xbe, string::binary>>
   end
 
-  test "encode a long string" do
+  test "encode() a long string" do
     value = "asdlfkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkj"
       <> "asdflkajsdflkjasdflkjasdlksjadflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasd"
-    <<indicator, size::little-unsigned-size(64), string::binary>> = VelocyPack.encode(value)
+    <<indicator, size::little-unsigned-size(64), string::binary>> = Encoder.encode(value)
 
     assert indicator == 0xbf
     assert size == byte_size(value)
     assert string == value
+  end
+
+  test "encode_with_size() an empty bit string" do
+    assert Encoder.encode_with_size(<<>>) == {0x40, 1}
+  end
+
+  test "encode_with_size() an empty string" do
+    assert Encoder.encode_with_size("") == {0x40, 1}
+  end
+
+  test "encode_with_size() a short string" do
+    assert Encoder.encode_with_size("a") == {<<0x41, "a">>, 2}
+    assert Encoder.encode_with_size("foo") == {<<0x43, "foo">>, 4}
+
+    string = "asdlfkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdfasdflkajsdflkjas"
+      <> "dfasdfasdfasdfasdfalka"
+    assert Encoder.encode_with_size(string) == {<<0xbe, string::binary>>, 127}
+  end
+
+  test "encode_with_size() a long string" do
+    value = "asdlfkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkj"
+      <> "asdflkajsdflkjasdflkjasdlksjadflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasd"
+    {<<indicator, size::little-unsigned-size(64), string::binary>>, encode_size} = Encoder.encode_with_size(value)
+
+    assert indicator == 0xbf
+    assert size == byte_size(value)
+    assert string == value
+    assert encode_size == 221
   end
 end
