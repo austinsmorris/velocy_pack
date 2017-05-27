72632ba0c377728afae47338a7036b95cd76c444
Austin Morris <austin.morris@gmail.com>
2017-05-26 22:30:04 -0400

    integer encode_with_size()

--- lib/velocy_pack/encoder/integer.ex
+++ lib/velocy_pack/encoder/integer.ex
@@ -46,6 +46,44 @@ defimpl VelocyPack.Encoder, for: Integer do
   def encode_with_size(0, _), do: {0x30, 1}
   def encode_with_size(1, _), do: {0x31, 1}
   def encode_with_size(2, _), do: {0x32, 1}
+  def encode_with_size(3, _), do: {0x33, 1}
+  def encode_with_size(4, _), do: {0x34, 1}
+  def encode_with_size(5, _), do: {0x35, 1}
+  def encode_with_size(6, _), do: {0x36, 1}
+  def encode_with_size(7, _), do: {0x37, 1}
+  def encode_with_size(8, _), do: {0x38, 1}
+  def encode_with_size(9, _), do: {0x39, 1}
+
+  def encode_with_size(-6, _), do: {0x3a, 1}
+  def encode_with_size(-5, _), do: {0x3b, 1}
+  def encode_with_size(-4, _), do: {0x3c, 1}
+  def encode_with_size(-3, _), do: {0x3d, 1}
+  def encode_with_size(-2, _), do: {0x3e, 1}
+  def encode_with_size(-1, _), do: {0x3f, 1}
+
+  def encode_with_size(value, _) when value < -9_223_372_036_854_775_808 do
+    raise "Cannot encode integers less than -9_223_372_036_854_775_808."
+  end
+
+  def encode_with_size(value, _) when value < -36_028_797_018_963_968, do: {<<0x27, value::little-signed-size(64)>>, 9}
+  def encode_with_size(value, _) when value < -140_737_488_355_328, do: {<<0x26, value::little-signed-size(56)>>, 8}
+  def encode_with_size(value, _) when value < -549_755_813_888, do: {<<0x25, value::little-signed-size(48)>>, 7}
+  def encode_with_size(value, _) when value < -2_147_483_648, do: {<<0x24, value::little-signed-size(40)>>, 6}
+  def encode_with_size(value, _) when value < -8_388_608, do: {<<0x23, value::little-signed-size(32)>>, 5}
+  def encode_with_size(value, _) when value < -32_768, do: {<<0x22, value::little-signed-size(24)>>, 4}
+  def encode_with_size(value, _) when value < -128, do: {<<0x21, value::little-signed-size(16)>>, 3}
+  def encode_with_size(value, _) when value < 0, do: {<<0x20, value::little-signed-size(8)>>, 2}
 
   def encode_with_size(value, _) when value <= 255, do: {<<0x28, value::little-unsigned-size(8)>>, 2}
+  def encode_with_size(value, _) when value <= 65_535, do: {<<0x29, value::little-unsigned-size(16)>>, 3}
+  def encode_with_size(value, _) when value <= 16_777_215, do: {<<0x2a, value::little-unsigned-size(24)>>, 4}
+  def encode_with_size(value, _) when value <= 4_294_967_295, do: {<<0x2b, value::little-unsigned-size(32)>>, 5}
+  def encode_with_size(value, _) when value <= 1_099_511_627_775, do: {<<0x2c, value::little-unsigned-size(40)>>, 6}
+  def encode_with_size(value, _) when value <= 281_474_976_710_655, do: {<<0x2d, value::little-unsigned-size(48)>>, 7}
+  def encode_with_size(value, _) when value <= 72_057_594_037_927_935, do: {<<0x2e, value::little-unsigned-size(56)>>, 8}
+  def encode_with_size(value, _) when value <= 18_446_744_073_709_551_615, do: {<<0x2f, value::little-unsigned-size(64)>>, 9}
+
+  def encode_with_size(_, _) do
+    raise "Cannot encode integers greater than 18_446_744_073_709_551_615."
+  end
 end
