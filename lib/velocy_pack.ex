2f74df34565157c1491399d377f918ee0c39d3c0
Austin Morris <austin.morris@gmail.com>
2017-05-30 11:21:11 -0400

    updated VelocyPack api

--- lib/velocy_pack.ex
+++ lib/velocy_pack.ex
@@ -6,6 +6,33 @@ defmodule VelocyPack do
   alias VelocyPack.Encoder
 
   def encode(value, options \\ []) do
+    try do
+      {:ok, encode_to_iodata!(value, options) |> IO.iodata_to_binary()}
+    rescue
+      error -> {:error, error}
+    end
+  end
+
+  def encode!(value, options \\ []) do
+    encode_to_iodata!(value, options) |> IO.iodata_to_binary()
+  end
+
+  def encode_to_iodata(value, options \\ []) do
+    try do
+      {:ok, encode_to_iodata!(value, options)}
+    rescue
+      error -> {:error, error}
+    end
+  end
+
+  def encode_to_iodata!(value, options \\ []) do
     Encoder.encode(value, options)
+  end
+
+  def decode(iodata, options \\ []) do
+    # todo - implement
+  end
+  def decode!(iodata, options \\ []) do
+    # todo - implement
   end
 end
--- test/velocy_pack_test.exs
+++ test/velocy_pack_test.exs
@@ -3,7 +3,35 @@ defmodule VelocyPackTest do
 
   use ExUnit.Case, async: true
 
-  test "encode/2 encodes to VPack" do
-    assert VelocyPack.encode(nil) == 0x18
+  test "encode/2 encodes to binary VelocyPack" do
+    assert VelocyPack.encode(nil) == {:ok, <<0x18>>}
+  end
+
+  test "encode/2 with error" do
+    assert {:error, %Protocol.UndefinedError{} = error} = VelocyPack.encode(make_ref())
+  end
+
+  test "encode!/2 encodes to binary VelocyPack" do
+    assert VelocyPack.encode!(nil) == <<0x18>>
+  end
+
+  test "encode!/2 will raise with error" do
+    assert_raise(Protocol.UndefinedError, fn() -> VelocyPack.encode!(make_ref()) end)
+  end
+
+  test "encode_to_iodata/2 encodes to an iodata list of VelocyPack" do
+    assert VelocyPack.encode_to_iodata(nil) == {:ok, [0x18]}
+  end
+
+  test "encode_to_iodata/2 with error" do
+    assert {:error, %Protocol.UndefinedError{} = error} = VelocyPack.encode_to_iodata(make_ref())
+  end
+
+  test "encode_to_iodata!/2 encodes to an iodata list of VelocyPack" do
+    assert VelocyPack.encode_to_iodata!(nil) == [0x18]
+  end
+
+  test "encode_to_iodata!/2 will raise with error" do
+    assert_raise(Protocol.UndefinedError, fn() -> VelocyPack.encode_to_iodata!(make_ref()) end)
   end
 end
