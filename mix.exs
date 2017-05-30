05abb7a4cf5d9b3d352d755bce577ac5dc3e7231
Austin Morris <austin.morris@gmail.com>
2017-05-30 11:43:37 -0400

    release 0.0.2

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule VelocyPack.Mixfile do
   def project do
     [
       app: :velocy_pack,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
