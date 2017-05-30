198562fc0a3dd544e96fc87c2015a5eb34ac7953
Austin Morris <austin.morris@gmail.com>
2017-05-30 08:02:35 -0400

    encode atoms to io data

--- lib/velocy_pack/encoder/atom.ex
+++ lib/velocy_pack/encoder/atom.ex
@@ -1,7 +1,7 @@
 defimpl VelocyPack.Encoder, for: Atom do
-  def encode(nil, _), do: 0x18
-  def encode(false, _), do: 0x19
-  def encode(true, _), do: 0x1a
+  def encode(nil, _), do: [0x18]
+  def encode(false, _), do: [0x19]
+  def encode(true, _), do: [0x1a]
 
   def encode_with_size(nil, _), do: {0x18, 1}
   def encode_with_size(false, _), do: {0x19, 1}
