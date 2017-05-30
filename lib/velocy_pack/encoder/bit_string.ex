753b1dd9346e33f0cce3b0e9e0dec3268c5b335b
Austin Morris <austin.morris@gmail.com>
2017-05-30 08:04:58 -0400

    encode bit strings as io data

--- lib/velocy_pack/encoder/bit_string.ex
+++ lib/velocy_pack/encoder/bit_string.ex
@@ -1,10 +1,10 @@
 defimpl VelocyPack.Encoder, for: BitString do
-  def encode(<<>>, _), do: 0x40
+  def encode(<<>>, _), do: [0x40]
   def encode(value, _) when is_binary(value) and byte_size(value) <= 126 do
-    <<(byte_size(value) + 0x40), value::binary>>
+    [<<(byte_size(value) + 0x40), value::binary>>]
   end
   def encode(value, _) when is_binary(value) do
-    <<0xbf, byte_size(value)::little-unsigned-size(64), value::binary>>
+    [<<0xbf, byte_size(value)::little-unsigned-size(64), value::binary>>]
   end
 
   def encode_with_size(<<>>, _), do: {0x40, 1}
--- test/velocy_pack/encoder/bit_string_test.exs
+++ test/velocy_pack/encoder/bit_string_test.exs
@@ -4,26 +4,26 @@ defmodule VelocyPack.Encoder.BitStringTest do
   use ExUnit.Case, async: true
 
   test "encode() an empty bit string" do
-    assert Encoder.encode(<<>>) == 0x40
+    assert Encoder.encode(<<>>) == [0x40]
   end
 
   test "encode() an empty string" do
-    assert Encoder.encode("") == 0x40
+    assert Encoder.encode("") == [0x40]
   end
 
   test "encode() a short string" do
-    assert Encoder.encode("a") == <<0x41, "a">>
-    assert Encoder.encode("foo") == <<0x43, "foo">>
+    assert Encoder.encode("a") == [<<0x41, "a">>]
+    assert Encoder.encode("foo") == [<<0x43, "foo">>]
 
     string = "asdlfkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdfasdflkajsdflkjas"
       <> "dfasdfasdfasdfasdfalka"
-    assert Encoder.encode(string) == <<0xbe, string::binary>>
+    assert Encoder.encode(string) == [<<0xbe, string::binary>>]
   end
 
   test "encode() a long string" do
     value = "asdlfkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkj"
       <> "asdflkajsdflkjasdflkjasdlksjadflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasdflkjasd"
-    <<indicator, size::little-unsigned-size(64), string::binary>> = Encoder.encode(value)
+    [<<indicator, size::little-unsigned-size(64), string::binary>>] = Encoder.encode(value)
 
     assert indicator == 0xbf
     assert size == byte_size(value)
