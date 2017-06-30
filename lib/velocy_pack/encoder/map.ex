f4b613b51a7bdf84055ae7ffeb5588306efd733c
Austin Morris <austin.morris@gmail.com>
2017-06-30 17:25:46 -0400

    initial, simple map encodeing without key sorting

--- lib/velocy_pack/encoder/map.ex
+++ lib/velocy_pack/encoder/map.ex
@@ -1,5 +1,41 @@
 defimpl VelocyPack.Encoder, for: Map do
-  def encode(%{}, _), do: [0x0a]
+  alias VelocyPack.Encoder
 
-  def encode_with_size(%{}, _), do: {0x0a, 1}
+  def encode(%{} = map, _) when map === %{}, do: [0x0a]
+  def encode(value, options) do
+    {total_size, nritems, reversed_list_with_sizes} = Enum.reduce(value, {0, 0, []}, fn({k, v}, {ls, n, rlws}) ->
+      {key, key_size} = Encoder.encode_with_size(k, options)
+      {value, value_size} = Encoder.encode_with_size(v, options)
+      {ls + key_size + value_size, n + 1, [{{key, key_size}, {value, value_size}} | rlws]}
+    end)
+
+    create_map_with_index_table(total_size, nritems, reversed_list_with_sizes)
+  end
+
+  def encode_with_size(%{} = map, _) when map === %{}, do: {0x0a, 1}
+  def encode_with_size(value, options) do
+    encoded = Encoder.encode(value, options)
+    {encoded, get_encoded_size(encoded)}
+  end
+
+  defp create_map_with_index_table(total_size, nritems, reversed_list_with_sizes) do # todo - when nritems/list_size?
+    {map, index_table, _} = Enum.reduce(
+      reversed_list_with_sizes,
+      {[], [], total_size + 3},
+      fn({{key, key_size}, {value, value_size}}, {iodata, it, acc}) ->
+        {[[key, value] | iodata], [acc - key_size - value_size | it], acc - key_size - value_size}
+      end
+    )
+
+    # todo = sort the index table by key
+    [
+      0x0b,
+      <<(total_size + nritems + 3)::little-unsigned-size(8)>>,
+      <<nritems::little-unsigned-size(8)>>,
+      map,
+      index_table
+    ]
+  end
+
+  defp get_encoded_size([0x0b, <<size::little-unsigned-size(8)>> | _]), do: size
 end
--- test/velocy_pack/encoder/map_test.exs
+++ test/velocy_pack/encoder/map_test.exs
@@ -7,6 +7,11 @@ defmodule VelocyPack.Encoder.MapTest do
     assert Encoder.encode(%{}) == [0x0a]
   end
 
+  test "encode() with non-empty map" do
+    assert Encoder.encode(%{b: true, a: 12, c: "xyz"}) ==
+      [11, <<19>>, <<3>>, [["Aa", "(\f"], ["Ab", 26], ["Ac", "Cxyz"]], [3, 7, 10]]
+  end
+
   test "encode_with_size() with empty map" do
     assert Encoder.encode_with_size(%{}) == {0x0a, 1}
   end
