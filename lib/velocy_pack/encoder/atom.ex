f99fea9f2e011402e34ac5c9c1cbdbf382cf67a3
Austin Morris <austin.morris@gmail.com>
2018-03-18 22:43:34 -0400

    cleanup and format Encoder implementation for Atom

--- lib/velocy_pack/encoder/atom.ex
+++ lib/velocy_pack/encoder/atom.ex
@@ -1,11 +1,35 @@
 defimpl VelocyPack.Encoder, for: Atom do
+  @moduledoc """
+  Implementation of the VelocyPack.Encoder protocol for Atom.
+  """
+
+  alias VelocyPack.Encoder
+
+  def encode(value, opts \\ [])
+
   def encode(nil, _), do: [0x18]
+
   def encode(false, _), do: [0x19]
-  def encode(true, _), do: [0x1a]
-  def encode(atom, options), do: VelocyPack.Encoder.encode(Atom.to_string(atom), options)
+
+  def encode(true, _), do: [0x1A]
+
+  def encode(value, opts) do
+    value
+    |> Atom.to_string()
+    |> Encoder.encode(opts)
+  end
+
+  def encode_with_size(value, opts \\ [])
 
   def encode_with_size(nil, _), do: {0x18, 1}
+
   def encode_with_size(false, _), do: {0x19, 1}
-  def encode_with_size(true, _), do: {0x1a, 1}
-  def encode_with_size(atom, options), do: VelocyPack.Encoder.encode_with_size(Atom.to_string(atom), options)
+
+  def encode_with_size(true, _), do: {0x1A, 1}
+
+  def encode_with_size(value, opts) do
+    value
+    |> Atom.to_string()
+    |> Encoder.encode_with_size(opts)
+  end
 end
--- test/velocy_pack/encoder/atom_test.exs
+++ test/velocy_pack/encoder/atom_test.exs
@@ -4,40 +4,34 @@ defmodule VelocyPack.Encoder.AtomTest do
   use ExUnit.Case, async: true
 
   test "encode() nil" do
-    assert Encoder.encode(nil) == [0x18]
-    assert Encoder.encode(:nil) == [0x18]
+    assert Encoder.encode(nil) === [0x18]
   end
 
   test "encode() false" do
-    assert Encoder.encode(false) == [0x19]
-    assert Encoder.encode(:false) == [0x19]
+    assert Encoder.encode(false) === [0x19]
   end
 
   test "encode() true" do
-    assert Encoder.encode(true) == [0x1a]
-    assert Encoder.encode(:true) == [0x1a]
+    assert Encoder.encode(true) === [0x1A]
   end
 
   test "encode() an atom" do
-    assert Encoder.encode(:foo) == Encoder.encode("foo")
+    assert Encoder.encode(:foo) === Encoder.encode("foo")
   end
 
   test "encode_with_size() nil" do
-    assert Encoder.encode_with_size(nil) == {0x18, 1}
-    assert Encoder.encode_with_size(:nil) == {0x18, 1}
+    assert Encoder.encode_with_size(nil) === {0x18, 1}
   end
 
   test "encode_with_size() false" do
-    assert Encoder.encode_with_size(false) == {0x19, 1}
-    assert Encoder.encode_with_size(:false) == {0x19, 1}
+    assert Encoder.encode_with_size(false) === {0x19, 1}
   end
 
   test "encode_with_size() true" do
-    assert Encoder.encode_with_size(true) == {0x1a, 1}
-    assert Encoder.encode_with_size(:true) == {0x1a, 1}
+    assert Encoder.encode_with_size(true) === {0x1A, 1}
   end
 
   test "encode_with_size() an atom" do
-    assert Encoder.encode_with_size(:foo) == Encoder.encode_with_size("foo")
+    assert Encoder.encode_with_size(:foo) === Encoder.encode_with_size("foo")
   end
 end
