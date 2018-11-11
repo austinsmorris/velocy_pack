34272f09caa325fb75b0c0131907d769216ff39b
Austin Morris <austin.morris@gmail.com>
2018-11-10 21:16:26 -0500

    test encoding empty map

--- test/velocy_pack/encode_test.exs
+++ test/velocy_pack/encode_test.exs
@@ -8,6 +8,11 @@ defmodule VelocyPack.EncodeTest do
       {:ok, encoded} = Encode.encode([])
       assert encoded == 0x01
     end
+
+    test "encodes an empty map" do
+      {:ok, encoded} = Encode.encode(%{})
+      assert encoded == 0x0A
+    end
   end
 
   describe "encode_list/1" do
@@ -21,6 +26,20 @@ defmodule VelocyPack.EncodeTest do
     test "encodes an empty list" do
       encoded = Encode.encode_list_with_size([])
       assert encoded == {0x01, 1}
+    end
+  end
+
+  describe "encode_map/1" do
+    test "encodes an empty map" do
+      encoded = Encode.encode_map(%{})
+      assert encoded == 0x0A
+    end
+  end
+
+  describe "encode_map_with_size/1" do
+    test "encodes an empty map" do
+      encoded = Encode.encode_map_with_size(%{})
+      assert encoded == {0x0A, 1}
     end
   end
 end
