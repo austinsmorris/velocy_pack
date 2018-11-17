8e6798292d2277cfeeb768a6a85fa3780cfe28fb
Austin Morris <austin.morris@gmail.com>
2018-11-16 22:27:47 -0500

    remove unnecesary atom encoder test

--- test/velocy_pack/encoder/atom_test.exs
+++ test/velocy_pack/encoder/atom_test.exs
@@ -15,18 +15,6 @@ defmodule VelocyPack.Encoder.AtomTest do
     assert Encoder.encode(true) === 0x1A
   end
 
-  test "encode/2 atom nil" do
-    assert Encoder.encode(:nil) === 0x18
-  end
-
-  test "encode/2 atom false" do
-    assert Encoder.encode(:false) === 0x19
-  end
-
-  test "encode/2 atom true" do
-    assert Encoder.encode(:true) === 0x1A
-  end
-
   test "encode/2 an atom" do
     assert Encoder.encode(:foo) === Encoder.encode("foo")
   end
@@ -41,18 +29,6 @@ defmodule VelocyPack.Encoder.AtomTest do
 
   test "encode_with_size/2 true" do
     assert Encoder.encode_with_size(true) === {0x1A, 1}
-  end
-
-  test "encode_with_size/2 atom nil" do
-    assert Encoder.encode_with_size(:nil) === {0x18, 1}
-  end
-
-  test "encode_with_size/2 atom false" do
-    assert Encoder.encode_with_size(:false) === {0x19, 1}
-  end
-
-  test "encode_with_size/2 atom true" do
-    assert Encoder.encode_with_size(:true) === {0x1A, 1}
   end
 
   test "encode_with_size/2 an atom" do
