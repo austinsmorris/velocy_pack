56ee17ded5e4eea9df2ae088ca9f6fca5e2ac482
Austin Morris <austin.morris@gmail.com>
2018-11-18 20:57:32 -0500

    update travis profile

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule VelocyPack.Mixfile do
     [
       app: :velocy_pack,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       name: "VelocyPack",
